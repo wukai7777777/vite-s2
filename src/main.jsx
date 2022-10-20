8361464f1b16525425934c89e1a1c0d50081e0d3
wukai <wukai@hengshi.com>
2022-10-20 20:31:12 +0800

    提交package

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
-import App from './App1'
+// import App from './App1'
+import App from './AppS2'
 import Pivot from './Pivot'
 import Dropdown from './Dropdown';
 import './index.css'
